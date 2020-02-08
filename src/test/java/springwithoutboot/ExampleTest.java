cf236e1cb5283a0892e327f9fbaf5e44a4358b9b
Christoffer G. Thomsen <chris@cgt.name>
2020-02-08 14:13:00 +0100

    Set up basic Maven project

---
+++ src/test/java/springwithoutboot/ExampleTest.java
@@ -0,0 +1,12 @@
+package springwithoutboot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+    @Test
+    void anything() {
+        assertEquals(4, 2+2);
+    }
+}
