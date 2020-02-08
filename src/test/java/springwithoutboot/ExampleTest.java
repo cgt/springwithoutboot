95d102c35b9a9b731bbfd449d3361e1ccf5b9c74
Christoffer G. Thomsen <chris@cgt.name>
2020-02-08 14:28:53 +0100

    Create context with a String bean

--- src/test/java/springwithoutboot/ExampleTest.java
+++ src/test/java/springwithoutboot/ExampleTest.java
@@ -3,6 +3,8 @@ package springwithoutboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -12,5 +14,22 @@ public class ExampleTest {
     void create_application_context() {
         final ApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
         assertNotNull(context);
+    }
+
+    @Test
+    void create_application_context_with_bean() {
+        final ApplicationContext context = new AnnotationConfigApplicationContext(ConfigWithBean.class);
+
+        final var stringBean = context.getBean(String.class);
+
+        assertEquals("Hello, World!", stringBean);
+    }
+
+    @Configuration
+    public static class ConfigWithBean {
+        @Bean
+        public String aString() {
+            return "Hello, World!";
+        }
     }
 }
