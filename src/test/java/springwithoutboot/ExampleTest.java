4a968425c16a228801649810a4aa42dbd19af0e3
Christoffer G. Thomsen <chris@cgt.name>
2020-02-08 14:25:18 +0100

    Construct app context in test

--- src/test/java/springwithoutboot/ExampleTest.java
+++ src/test/java/springwithoutboot/ExampleTest.java
@@ -1,12 +1,21 @@
 package springwithoutboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ExampleTest {
     @Test
     void anything() {
         assertEquals(4, 2+2);
+    }
+
+    @Test
+    void create_application_context() {
+        final ApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        assertNotNull(context);
     }
 }
